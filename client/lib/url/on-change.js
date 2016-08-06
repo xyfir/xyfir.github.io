081aadaa54508b7cae66166eb546cd61fbbb9ae5
MrXyfir <mr@xyfir.com>
2016-08-05 18:46:40 -0700

    Add url modules

---
+++ client/lib/url/on-change.js
@@ -0,0 +1,40 @@
+import RouteParser from "route-parser";
+
+// Constants
+import routes from "constants/routes";
+import { HOME } from "constants/views";
+
+// Modules
+import parseQuery from "./parse-query-string";
+
+export default function() {
+
+    let view = "", hash = {
+        route: location.hash.substr(1).split('?')[0],
+        params: {}, query: {}
+    };
+
+    // Match current route
+    for (let route of routes) {
+        let p = (new RouteParser(route[0])).match(hash.route);
+
+        if (p !== false) {
+            hash.params = p;
+            view = route[1];
+            break;
+        }
+    }
+
+    // Set default view / route
+    if (!view) {
+        view = HOME;
+        hash.route = "/";
+    }
+    // Parse query string
+    else {
+        hash.query = parseQuery();
+    }
+
+    return { view, hash };
+
+}
