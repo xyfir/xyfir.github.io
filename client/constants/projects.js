a3bca7bc330c8bcd2026e673103ab59a530a1a7a
MrXyfir <mr@xyfir.com>
2016-08-05 19:21:19 -0700

    Add projects.js

---
+++ client/constants/projects.js
@@ -0,0 +1,92 @@
+const projects = {
+    
+    "xyfir-accounts": {
+        name: "Xyfir Accounts",
+        link: "https://accounts.xyfir.com/",
+        group: ["Xyfir"],
+        description: "Account management system for the Xyfir Network.",
+        documentation: {
+            "integration": {
+                name: "Integration For Developers",
+                location: "xyfir/accounts/integration",
+                description: "Learn how to integrate Xyfir Accounts into your site or app."
+            }
+        }
+    },
+
+    "xyfir-ads": {
+        name: "Xyfir Ads",
+        link: "https://ads.xyfir.com/",
+        group: ["Xyfir"],
+        description: "Native advertisement system for advertisers and publishers.",
+        documentation: {
+            "developers": {
+                name: "For Developers",
+                location: "xyfir/ads/developers",
+                description: "Learn how to integrate Xyfir Ads into your site, app, etc."
+            },
+            "advertisers": {
+                name: "For Advertisers",
+                location: "xyfir/ads/advertisers",
+                description: "Learn how to manage your account and advertisement campaigns."
+            },
+            "publishers": {
+                name: "For Publishers",
+                location: "xyfir/ads/publishers",
+                description: "Learn how to manage your account and publisher campaigns."
+            },
+            "users": {
+                name: "For Users",
+                location: "xyfir/ads/users",
+                description: "Xyfir Ads was developed with the end user in mind. Learn about what makes us different."
+            }
+        }
+    },
+
+    "libyq": {
+        name: "Libyq",
+        link: "https://libyq.com/",
+        group: ["Other", "Libyq"],
+        description: "An all-in-one cloud-synced ebook reader, storage, and management system.",
+        documentation: {}
+    },
+
+    "libyq-annotations": {
+        name: "Libyq annotations",
+        link: "https://annotations.libyq.com/",
+        group: ["Other", "Libyq"],
+        description: "A single platform for storing, finding, and managing interactive annotations for ebooks.",
+        documentation: {
+            "integration": {
+                name: "Integration For Ebook Reader Developers",
+                location: "libyq/annotations/integration",
+                description: "Learn how to integrate Libyq Annotations into your ebook reader."
+            }
+        }
+    },
+
+    "ptorx": {
+        name: "Ptorx",
+        link: "https://ptorx.com/",
+        group: ["Other"],
+        description: "An extra layer of security, privacy, and control for your emails.",
+        documentation: {}
+    },
+
+    "vynote": {
+        name: "Vynote",
+        link: "https://vynote.com/",
+        group: ["Other"],
+        description: "A productive way to store, organize, and edit your ideas, code, snippets, and lists.",
+        documentation: {
+            "help": {
+                name: "User Help Docs",
+                location: "vynote/help",
+                description: "Better understand certain features within Vynote."
+            }
+        }
+    }
+
+};
+
+export default projects;
