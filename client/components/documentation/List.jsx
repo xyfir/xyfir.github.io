67298bb598314fecceead7aa1f3408b2c1b078db
MrXyfir <mr@xyfir.com>
2016-09-04 09:34:17 -0700

    Output legal documentation

--- client/components/documentation/List.jsx
+++ client/components/documentation/List.jsx
@@ -22,9 +22,11 @@ export default class DocumentationList extends React.Component {
                     return (
                         <li className="project">
                             {pO.name}
-                            <ul>{!Object.keys(pO.documentation).length ? (
+                            <ul>{Object.keys(pO.documentation).length == 1 ? (
                                 <li>No documentation</li>
                             ) : Object.keys(pO.documentation).map(d => {
+                                if (d == "legal") return <span />;
+                                
                                 const dO = pO.documentation[d];
 
                                 return (
@@ -41,6 +43,34 @@ export default class DocumentationList extends React.Component {
                         </li>
                     );
                 })}</ul>
+
+                <h2>Legal Documentation</h2>
+
+                <hr />
+
+                <ul className="projects">{Object.keys(projects).map(p => {
+                    const pO = projects[p];
+
+                    return (
+                        <li className="project">
+                            {pO.name}
+                            <ul>{
+                                Object.keys(pO.documentation.legal).map(d => {
+                                    const dO = pO.documentation.legal[d];
+
+                                    return (
+                                        <li className="documentation-item">
+                                            <a href={`#/documentation/${p}/${d}`}>{
+                                                dO.name
+                                            }</a>
+                                        </li>
+                                    );
+                                })
+                            }</ul>
+                        </li>
+                    );
+                })}</ul>
+
             </section>
         );
     }
