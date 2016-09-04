3a5ec83ebd5ae9486cc6abf16dc7bee36d9212cf
MrXyfir <mr@xyfir.com>
2016-09-04 09:34:47 -0700

    Simplify 'About' page description

--- client/components/pages/About.jsx
+++ client/components/pages/About.jsx
@@ -12,7 +12,7 @@ export default class About extends React.Component {
                 <h2>About</h2>
                 <hr />
                 <p>
-                    Planning for Xyfir and its network began in late 2014. xyfir.com was purchased in March of 2015 and Xyfir, LLC was formed in August 2015. By October 2015 work had begun on various portions of the network. A year after the company was created, small portions of the network began being made available to the public. Development and planning continues with ambitious goals for the future.
+                    The Xyfir Network is operated by Xyfir, LLC. This website is a work in progress.
                 </p>
             </section>
         );
