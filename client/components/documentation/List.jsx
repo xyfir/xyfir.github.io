13ebf146ec47aed349eac1d99bfb23243bb38f56
MrXyfir <mr@xyfir.com>
2017-01-18 13:33:05 -0800

    Use MUI tabs to split normal and legal docs

--- client/components/documentation/List.jsx
+++ client/components/documentation/List.jsx
@@ -1,71 +1,83 @@
 import React from "react";
 
+// MUI Components
+import { Tabs, Tab } from "material-ui/Tabs";
+
 export default class DocumentationList extends React.Component {
 
     constructor(props) {
         super(props);
     }
 
-    render() {
-        const projects = this.props.projects;
+    renderDocs(projects) {
+        return Object.keys(projects).map(p => {
+            const pO = projects[p];
 
+            return (
+                <li className="project">
+                    {pO.name}
+                    <ul>{Object.keys(pO.documentation).length == 1 ? (
+                        <li>No documentation</li>
+                    ) : Object.keys(pO.documentation).map(d => {
+                        if (d == "legal") return <span />;
+                        
+                        const dO = pO.documentation[d];
+
+                        return (
+                            <li className="documentation-item">
+                                <a href={`#/documentation/${p}/${d}`}>{
+                                    dO.name
+                                }</a>
+                                <span className="description">{
+                                    dO.description
+                                }</span>
+                            </li>
+                        );
+                    })}</ul>
+                </li>
+            );
+        });
+    }
+
+    renderLegalDocs(projects) {
+        return Object.keys(projects).map(p => {
+            const pO = projects[p];
+
+            return (
+                <li className="project">
+                    {pO.name}
+                    <ul>{Object.keys(pO.documentation.legal).map(d => {
+                        const dO = pO.documentation.legal[d];
+
+                        return (
+                            <li className="documentation-item">
+                                <a href={`#/documentation/${p}/${d}`}>{
+                                    dO.name
+                                }</a>
+                            </li>
+                        );
+                    })}</ul>
+                </li>
+            );
+        });
+    }
+
+    render() {
         return (
             <section className="documentation-list">
-                <h2>Documentation List</h2>
-                
-                <ul className="projects">{Object.keys(projects).map(p => {
-                    const pO = projects[p];
+                <Tabs className="tabs">
+                    <Tab label="Documentation">
+                        <ul className="projects">{
+                            this.renderDocs(this.props.projects)
+                        }</ul>
+                    </Tab>
 
-                    return (
-                        <li className="project">
-                            {pO.name}
-                            <ul>{Object.keys(pO.documentation).length == 1 ? (
-                                <li>No documentation</li>
-                            ) : Object.keys(pO.documentation).map(d => {
-                                if (d == "legal") return <span />;
-                                
-                                const dO = pO.documentation[d];
-
-                                return (
-                                    <li className="documentation-item">
-                                        <a href={`#/documentation/${p}/${d}`}>{
-                                            dO.name
-                                        }</a>
-                                        <span className="description">{
-                                            dO.description
-                                        }</span>
-                                    </li>
-                                );
-                            })}</ul>
-                        </li>
-                    );
-                })}</ul>
-
-                <h2>Legal Documentation</h2>
-
-                <ul className="projects">{Object.keys(projects).map(p => {
-                    const pO = projects[p];
-
-                    return (
-                        <li className="project">
-                            {pO.name}
-                            <ul>{
-                                Object.keys(pO.documentation.legal).map(d => {
-                                    const dO = pO.documentation.legal[d];
-
-                                    return (
-                                        <li className="documentation-item">
-                                            <a href={`#/documentation/${p}/${d}`}>{
-                                                dO.name
-                                            }</a>
-                                        </li>
-                                    );
-                                })
-                            }</ul>
-                        </li>
-                    );
-                })}</ul>
-
+                    <Tab label="Legal Docs">
+                        <ul className="projects">{
+                            this.renderLegalDocs(this.props.projects)
+                        }</ul>
+                    </Tab>
+                </Tabs>
             </section>
         );
     }
