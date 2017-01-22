ec32810dd1a5f077076d387b646c634823f93b06
MrXyfir <mr@xyfir.com>
2017-01-21 16:48:13 -0800

    Replace Gulp and Browserify with npm scripts and WebPack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  
+  entry: './client/components/App.jsx',
+  
+  output: {
+    filename: 'App.js',
+    path: './public/js'
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'client'),
+      "node_modules"
+    ],
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, 'client/components'),
+        path.resolve(__dirname, 'client/constants'),
+        path.resolve(__dirname, 'client/lib')
+      ],
+      exclude: /node_modules/,
+      options: {
+        presets: ['es2017', 'es2016', 'es2015', 'react']
+      }
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      compress: { unused: false }
+    })
+  ]
+
+};
