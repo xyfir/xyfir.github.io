b0df0ffa3020ae4b9844599c1d82bc8f795a5d8e
MrXyfir <mr@xyfir.com>
2017-07-21 16:09:52 -0700

    Improvements, changes
    - Update code style
    - Use superagent
    - No longer sends mail from the user's provided email

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,46 +1,51 @@
-const request = require("request");
-const config = require("config");
+const request = require('superagent');
+const config = require('config');
 
-const mailgun = require("mailgun-js")({
-	apiKey: config.keys.mailgun,
-	domain: config.addresses.mailgun.domain
+const mailgun = require('mailgun-js')({
+  apiKey: config.keys.mailgun,
+  domain: config.addresses.mailgun.domain
 });
 
 /*
-    POST api/contact
-    REQUIRED
-        tag: string, regarding: string, message: string, email: string,
-        recaptcha: string
-    RETURN
-        error: boolean
-    DESCRIPTION
-        Build a email using req.body and send to contact@xyfir.com
+  POST api/contact
+  REQUIRED
+    tag: string, regarding: string, message: string, email: string,
+    recaptcha: string
+  RETURN
+    error: boolean, message?: string
+  DESCRIPTION
+    Build a email using req.body and send to contact@xyfir.com
 */
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
+  const b = req.body;
+
+  try {
     // Check if reCaptcha response is valid
-    request.post({
-        url: "https://www.google.com/recaptcha/api/siteverify",
-        form: {
-            secret: config.keys.recaptcha,
-            response: req.body.recaptcha,
-            remoteip: req.ip
-        }
-    }, (e, r, body) => {
-        if (e || !JSON.parse(body).success) {
-            res.json({ error: true });
-        }
-        else {
-            // Email contact@xyfir.com with user's message'
-            mailgun.messages().send({
-                from: req.body.email, to: "contact@xyfir.com",
-                subject: req.body.regarding + " | " + req.body.tag,
-                text: req.body.message
-            }, (err, body) => {
-                res.json({ error: !!err });
-            });
-        }
+    const result = await request
+      .post('https://www.google.com/recaptcha/api/siteverify')
+      .type('form')
+      .send({
+        secret: config.keys.recaptcha,
+        response: b.recaptcha,
+        remoteip: req.ip
+      });
+    
+    if (!result.body.success) throw 'Invalid captcha';
+    
+    // Email contact@xyfir.com with user's message'
+    await mailgun.messages().send({
+      to: 'contact@xyfir.com',
+      from: 'contact@xyfir.com',
+      text: b.message,
+      subject: `${b.regarding} | ${b.tag} | ${b.email}`
     });
+
+    res.json({ error: false });
+  }
+  catch (err) {
+    res.json({ error: true, message: err });
+  }
 
 };
