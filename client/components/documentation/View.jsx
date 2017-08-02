48e8ecd633ff6485b8c74b4734de5852e9ae0895
MrXyfir <mr@xyfir.com>
2017-08-02 10:16:36 -0700

    Use superagent. Update code style

--- client/components/documentation/View.jsx
+++ client/components/documentation/View.jsx
@@ -1,68 +1,63 @@
-import marked from "marked";
-import React from "react";
+import request from 'superagent';
+import marked from 'marked';
+import React from 'react';
 
-// Modules
-import request from "lib/request";
-
-import DynamicIframe from "components/misc/DynamicIframe";
+import DynamicIframe from 'components/misc/DynamicIframe';
 
 export default class ViewDocumentation extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const p = this.props.projects[this.props.params.project];
+    const p = this.props.projects[this.props.params.project];
 
-        // Validate that project and documentation file exist
-        if (p === undefined) {
-            location.hash = "/documentation";
-        }
-        else {
-            const d = p.documentation[this.props.params.doc]
-                || p.documentation.legal[this.props.params.doc];
-
-            if (d === undefined)
-                location.hash = "/documentation";
-            else
-                this.state = { project: p, documentation: d };
-        }
+    // Validate that project and documentation file exist
+    if (p === undefined) {
+      location.hash = '#/documentation';
     }
+    else {
+      const d = p.documentation[this.props.params.doc] ||
+        p.documentation.legal[this.props.params.doc];
 
-    componentDidMount() {
-        const url = "https://api.github.com/repos/Xyfir/Documentation/contents/"
-            + this.state.documentation.location + ".md";
-
-        request(url, (res) => {
-            // Add CSS files
-            this.refs.frame.refs.frame.contentDocument.head.innerHTML = `
-                <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" type="text/css">
-                <link rel="stylesheet" href="static/css/style.css">
-            `;
-
-            // Convert markdown to html
-            this.refs.frame.refs.frame.contentDocument.body.innerHTML = `
-                <div class="markdown">${
-                    marked(
-                        window.atob(res.content), { santize: true }
-                    )
-                }</div>
-            `;
-        });
+      if (d === undefined)
+        location.hash = '#/documentation';
+      else
+        this.state = { project: p, documentation: d };
     }
+  }
 
-    render() {
-        return (
-            <section className="view-documentation">
-                <h2 className="title">
-                    {this.state.project.name}: {this.state.documentation.name}
-                </h2>
-                <span className="description">{
-                    this.state.documentation.description
-                }</span>
+  componentDidMount() {
+    request
+      .get(
+        'https://raw.githubusercontent.com/Xyfir/Documentation/master/' +
+        this.state.documentation.location + '.md'
+      )
+      .end((err, res) => {
+        // Add CSS files
+        this.refs.frame.refs.frame.contentDocument.head.innerHTML = `
+          <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' rel='stylesheet' type='text/css'>
+          <link rel='stylesheet' href='static/css/style.css'>
+        `;
 
-                <DynamicIframe ref="frame" className="documentation" />
-            </section>
-        );
-    }
+        // Convert markdown to html
+        this.refs.frame.refs.frame.contentDocument.body.innerHTML =
+          `<div class='markdown-body'>${marked(res.text, { santize: true })}</div>`;
+      });
+  }
+
+  render() {
+    return (
+      <section className='view-documentation'>
+        <h2 className='title'>
+          {this.state.project.name}: {this.state.documentation.name}
+        </h2>
+        <span className='description'>{
+          this.state.documentation.description
+        }</span>
+
+        <DynamicIframe ref='frame' className='documentation' />
+      </section>
+    );
+  }
 
 }
