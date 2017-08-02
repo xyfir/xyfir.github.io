93ca636f85f12434a97375a3001544704c5b8eda
MrXyfir <mr@xyfir.com>
2017-08-02 12:23:52 -0700

    Rename public/ to static/. Update code style

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require("app-module-path").addPath(__dirname);
+require('app-module-path').addPath(__dirname);
 
-const express = require("express");
-const parser = require("body-parser");
+const express = require('express');
+const parser = require('body-parser');
 const app = express();
 
-const config = require("./config");
+const config = require('./config');
 
 /* Body Parser */
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 /* Routes / Controllers */
-app.use("/static", express.static(__dirname + "/public"));
-app.use("/api", require("./controllers/"));
-app.get("/*", (req, res) => {
-    res.sendFile(__dirname + "/views/App.html");
-});
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/api', require('./controllers/'));
+app.get('/*', (req, res) =>
+  res.sendFile(__dirname + '/views/App.html')
+);
 
 /* Start Server */
-app.listen(config.environment.port, () => {
-    console.log("~~Server running on port", config.environment.port);
-});
+app.listen(config.environment.port, () =>
+  console.log('~~Server running on port', config.environment.port)
+);
