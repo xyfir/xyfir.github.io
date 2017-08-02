95fb6763649b87003a93962f9be980bab25b9808
MrXyfir <mr@xyfir.com>
2017-08-02 14:46:47 -0700

    Add config.default.js

---
+++ config.default.js
@@ -0,0 +1,16 @@
+exports.environment = {
+  type: '',
+  port: -1
+};
+
+exports.keys = {
+  recaptcha: '',
+  mailgun: ''
+};
+
+exports.addresses = {
+	mailgun: {
+		domain: '',
+		api: ''
+	}
+};
