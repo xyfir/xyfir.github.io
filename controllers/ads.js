50f8dcf344cf06225a8ddbbebdc693ae15aefb5b
MrXyfir <mr@xyfir.com>
2017-08-24 15:05:29 -0700

    Add GET api/ads

---
+++ controllers/ads.js
@@ -0,0 +1,29 @@
+const xyAds = require('xyfir-ads');
+
+/*
+  GET api/ads
+  OPTIONAL
+    count: number, xyfir:  boolean, blacklist: string, keywords: string
+  RETURN
+    [{ https://github.com/Xyfir/Ads/blob/master/ads.json }]
+  DESCRIPTION
+    Use xyfir-ads package from anywhere
+*/
+
+module.exports = async function(req, res) {
+
+  const options = {
+    count: +req.query.count || 1,
+    keywords: req.query.keywords
+      ? req.query.keywords.split(',')
+      : [],
+    blacklist: req.query.blacklist
+      ? req.query.blacklist.split(',').map(a => isNaN(+a) ? a : +a)
+      : []
+  };
+
+  if (req.query.xyfir) options.xyfir = !!+req.query.xyfir;
+
+  res.json(await xyAds(options));
+
+};
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+router.get('/ads', require('./ads'));
 router.post('/contact', require('./contact'));
 router.post('/advertise', require('./advertise'));
 
