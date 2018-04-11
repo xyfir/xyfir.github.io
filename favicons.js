0ce9be02a9ed5e4a742278e202d240b6563680f5
MrXyfir <mr@xyfir.com>
2018-04-11 15:27:25 -0700

    Support generating xyButtons/other favicons

--- favicons.js
+++ favicons.js
@@ -1,46 +1,78 @@
 const favicons = require('favicons');
-const package = require('./package.json');
 const util = require('util');
 const path = require('path');
 const fs = require('fs');
 
 const writeFile = util.promisify(fs.writeFile);
 
-favicons('icon.png', {
-  path: '/static/icons/',
-  appName: 'Xyfir',
-  appDescription: null,
-  developerName: 'Xyfir, LLC',
-  developerURL: 'https://www.xyfir.com',
-  dir: 'auto',
-  lang: 'en-US',
-  background: '#FFF',
-  theme_color: '#2196f3',
-  display: 'standalone',
-  orientation: 'any',
-  start_url: '/',
-  version: package.version,
-  logging: false,
-  icons: {
-    android: true,
-    appleIcon: true,
-    appleStartup: true,
-    coast: { offset: 25 },
-    favicons: true,
-    firefox: true,
-    windows: true,
-    yandex: true
-  }
-},
-async (error, response) => {
-  if (error) return console.error(error);
+/**
+ * @param {string} icon
+ * @param {string} output
+ * @param {string} name
+ */
+function generate(icon, output, name) {
+  return new Promise((resolve, reject) =>
+    favicons(`res/icons/${icon}`, {
+      path: `/${output}`,
+      appName: name,
+      appDescription: null,
+      developerName: 'Xyfir, LLC',
+      developerURL: 'https://www.xyfir.com',
+      dir: 'auto',
+      lang: 'en-US',
+      background: '#FFF',
+      theme_color: '#2196f3',
+      display: 'standalone',
+      orientation: 'any',
+      start_url: '/',
+      version: null,
+      logging: false,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: { offset: 25 },
+        favicons: true,
+        firefox: true,
+        windows: true,
+        yandex: true
+      }
+    },
+    async (error, response) => {
+      if (error) reject(error);
 
-  for (let img of response.images) {
-    await writeFile(
-      path.resolve(__dirname, 'static/icons/', img.name),
-      img.contents
-    );
+      for (let img of response.images) {
+        await writeFile(
+          path.resolve(__dirname, output, img.name),
+          img.contents
+        );
+      }
+
+      resolve();
+    })
+  );
+}
+
+(async function() {
+
+  const projects = [{
+    name: 'Xyfir',
+    icon: 'xyfir.png',
+    output: 'static/icons/'
+  }, {
+    name: 'xyButtons',
+    icon: 'xybuttons.png',
+    output: 'static/icons/xyfir-buttons/'
+  }];
+
+  for (let project of projects) {
+    try {
+      await generate(project.icon, project.output, project.name);
+      console.log('Icons generated for', project.name);
+    }
+    catch (err) {
+      console.error(err);
+    }
   }
 
-  console.log('Icons generated');
-});
+})();
