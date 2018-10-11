bf38b2cc90c369cc326d031416116ce659980ae5
MrXyfir <mr@xyfir.com>
2018-10-11 15:16:19 -0700

    Update routes

--- app.js
+++ app.js
@@ -11,12 +11,9 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 /* Routes / Controllers */
-app.get('/software-solutions', (req, res) =>
-  res.sendFile(__dirname + '/views/XSS.html')
-);
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/api', require('./controllers/'));
-app.get('/*', (req, res) => res.sendFile(__dirname + '/views/App.html'));
+app.get('/*', (req, res) => res.sendFile(__dirname + '/static/index.html'));
 
 /* Start Server */
 app.listen(config.environment.port, () =>
